b32b3f935d0e5b7dbec3a0feefd6da02c298ede3
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-03 10:58:22 +0530

    Time: 1 ms (99.82%) | Memory: 41.7 MB (86.26%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -2,9 +2,7 @@ class Solution {
     public int scoreOfString(String s) {
         int sum=0;
         for(int i=0;i<s.length()-1;i++){
-            int n1=s.charAt(i)-'a';
-            int n2=s.charAt(i+1)-'a';
-            sum+=Math.abs(n1-n2);
+            sum+=Math.abs(s.charAt(i)-s.charAt(i+1));
         }
         return sum;
     }
