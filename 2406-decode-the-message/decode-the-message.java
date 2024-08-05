011311f86c78ca50b5e4231d2c479bfb692c96d4
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-05 11:59:04 +0530

    Time: 19 ms (8.24%) | Memory: 45.2 MB (7.90%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        String str="abcdefghijklmnopqrstuvwxyz";
+        String res="";
+        key=key.replace(" ","");
+        System.out.println(key);
+        HashSet<Character> set=new LinkedHashSet<>();
+        for(char c:key.toCharArray()){
+            set.add(c);
+        }
+        key="";
+        for(char c:set){
+            key+=c;
+        }
+        for(char c:message.toCharArray()){
+            if(Character.isSpace(c)){
+                res+=" ";
+            }
+            else {
+                res+=str.charAt((key.indexOf(c)));
+            }
+        }
+        return res;
+    }
+}
