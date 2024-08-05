87cd2e49b4fe1efea99fc7e4ed168819470a4141
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-05 11:37:39 +0530

    Time: 1 ms (58.97%) | Memory: 41.8 MB (24.53%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
@@ -1,16 +1,16 @@
 class Solution {
     public String replaceDigits(String s) {
-        String str="";
+        StringBuilder str=new StringBuilder();
         char pre='0';
         for(char c:s.toCharArray()){
             if(Character.isDigit(c)){
                 int n=c-'0';
                 n=n+pre;
-                str+=(char)n;
+                str.append((char)n);
             }
-            else str+=c;
+            else str.append(c);
             pre=c;
         }
-        return str;
+        return str.toString();
     }
 }
