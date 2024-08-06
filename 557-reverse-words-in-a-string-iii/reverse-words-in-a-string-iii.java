743374e70c02d25f1eb61d4ee7b112d3da7ea29c
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-06 09:41:15 +0530

    Time: 15 ms (27.09%) | Memory: 45.4 MB (23.58%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,14 +1,33 @@
 class Solution {
+    String make(String str){
+        char c[]=str.toCharArray();
+        int i=0,j=c.length-1;
+        while(i<j){
+            char c1=c[i];
+            c[i]=c[j];
+            c[j]=c1;
+            i++;j--;
+        }
+        String st=new String(c);
+        return st;
+    }
     public String reverseWords(String s) {
-        String str="";
-        for(int i=s.length()-1;i>=0;i--){
-            str+=s.charAt(i);
+        // String str="";
+        // for(int i=s.length()-1;i>=0;i--){
+        //     str+=s.charAt(i);
+        // }
+        // String a[]=str.split(" ");
+        // s="";
+        // for(int i=a.length-1;i>=0;i--){
+        //     s+=a[i]+" ";
+        // }
+        // return s.trim();
+        String st="";
+        String arr[]=s.split(" ");
+        for(int i=0;i<arr.length;i++){
+            if(i==arr.length-1) st+=make(arr[i]);
+            else st+=make(arr[i])+" ";
         }
-        String a[]=str.split(" ");
-        s="";
-        for(int i=a.length-1;i>=0;i--){
-            s+=a[i]+" ";
-        }
-        return s.trim();
+        return st;
     }
 }
