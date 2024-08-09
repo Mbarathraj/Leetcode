e48a7c8065e65e5b6fb55cfa7e9cee31bff9fcf5
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-10 00:15:00 +0530

    Time: 7 ms (11.62%) | Memory: 44.4 MB (16.73%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,21 +1,21 @@
 class Solution {
     public int compress(char[] chars) {
         int c=1;
-        String str="";
+        StringBuilder str=new StringBuilder();
         for(int i=1;i<chars.length;i++){
             if(chars[i]==chars[i-1]) c++;
             else {
                 if(c>1)
-                 str+=chars[i-1]+""+c;
-                else  str+=chars[i-1];
+                 str.append(chars[i-1]+""+c);
+                else  str.append(chars[i-1]);
                 c=1;
             }
         }
         if(c>1)
-          str+=chars[chars.length-1]+""+c;
-        else  str+=chars[chars.length-1];
+          str.append(chars[chars.length-1]+""+c);
+        else  str.append(chars[chars.length-1]);
         int k=0;
-        for(char c1:str.toCharArray()){
+        for(char c1:str.toString().toCharArray()){
             chars[k++]=c1;
         }
         return k;
