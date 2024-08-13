1ca6cb40b08b6d24522c018ea7649ea8fc6e49fe
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-13 18:57:37 +0530

    Time: 3 ms (35.56%) | Memory: 41.7 MB (51.86%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,14 +1,15 @@
 import java.util.Collection;
+
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i:arr){
-            map.put(i,map.getOrDefault(i,0)+1);
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for(int i = 0 ; i < arr.length ; i++){
+            frequencyMap.put(arr[i], frequencyMap.getOrDefault(arr[i], 0) + 1);
         }
-        Collection<Integer> collection = map.values();
-        Set<Integer> set=new HashSet<>(collection);
-        System.out.println(map);
-        System.out.print(set);
-        return set.size()==map.size();
+        Collection<Integer> collectionOfFreqs = frequencyMap.values();
+
+        Set<Integer> setOfFrequencies = new HashSet<>(collectionOfFreqs);
+
+        return (setOfFrequencies.size()==collectionOfFreqs.size());
     }
 }
