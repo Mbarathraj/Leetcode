7eeb11a4c7e130a6984922e9b33f3bb6a9893b1c
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-15 01:08:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (63.02%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean rotateString(String A, String B) {
+        if(A == null || B == null) {
+            //throw exception on A and B both being null?
+            return false;
+        }
+        if(A.length() != B.length()) {
+            return false;
+        }
+        if(A.length() == 0) {
+            return true;
+        }
+        for(int i = 0; i < A.length(); i++) {
+            if(rotateString(A, B, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean rotateString(String A, String B, int rotation) {
+        for(int i = 0; i < A.length(); i++) {
+            if(A.charAt(i) != B.charAt((i+rotation)%B.length())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
