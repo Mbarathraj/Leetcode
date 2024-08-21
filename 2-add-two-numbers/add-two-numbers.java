5f4ec52d8aef2f229cd0379e6610ffaaf3d5f2f3
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-22 00:16:03 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (37.06%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,45 +10,29 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp1=l1,temp2=l2;
-        ArrayList<Integer> al=new ArrayList<>();
-        int c=0;
-        ListNode head=null,tail=null;
-        while(temp1!=null ||temp2!=null){
-            if(temp1!=null && temp2!=null){
-                int sum=temp1.val+temp2.val+c;
-                al.add(sum%10);
-                c=sum/10;
-                temp1=temp1.next;
-                temp2=temp2.next;
-            }
-           else{
-            if(temp1!=null){
-                int sum=temp1.val+c;
-                al.add(sum%10);
-                c=sum/10;
-                temp1=temp1.next;
-            }
-            else{
-                int sum=temp2.val+c;
-                al.add(sum%10);
-                c=sum/10;
-                temp2=temp2.next;
-            }
-           }
-           System.out.println("carry: "+c);
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            
+            int sum = carry + x + y;
+
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        if(c>0) al.add(c);
-        for(int i:al){
-            if(head==null){
-                head=tail=new ListNode(i);
-            }
-            else{
-                ListNode newnode=new ListNode(i);
-                tail.next=newnode;
-                tail=newnode;
-            }
+        if (carry > 0) {
+            current.next = new ListNode(carry);
         }
-        return head;
+        
+        return dummyHead.next;
     }
 }
