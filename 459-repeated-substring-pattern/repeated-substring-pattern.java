1c296899ca599975b4640f1bd991ab07a0ed5e24
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-08-31 18:39:17 +0530

    Time: 751 ms (5.05%) | Memory: 45.4 MB (22.11%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        for(int i=0;i<s.length();i++){
+            String s1=s.substring(0,i+1);
+            int c=0;
+            for(int j=i+1;j<s.length();j+=s1.length()){
+                if(j+s1.length()<=s.length()){
+                 if(s.substring(j,j+s1.length()).equals(s1)) c++;
+                 else break;
+                }
+            }
+            System.out.println(c);
+            if(s.length()/s1.length()==c+1 && c!=0 && s.length()%s1.length()==0) return true;
+        }
+        return false;
+    }
+}
