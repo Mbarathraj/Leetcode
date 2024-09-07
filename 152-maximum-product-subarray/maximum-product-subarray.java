c3d125654f1f27f5fa0ff220e03dc03eb9e9acf5
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-07 10:54:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (76.97%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,15 +1,33 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int ma=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            int sum=nums[i];
-            ma=Math.max(ma,sum);
-            for(int j=i+1;j<nums.length;j++){
-                sum*=nums[j];
-                ma=Math.max(ma,sum);
+        // int ma=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=nums[i];
+        //     ma=Math.max(ma,sum);
+        //     for(int j=i+1;j<nums.length;j++){
+        //         sum*=nums[j];
+        //         ma=Math.max(ma,sum);
+        //     }
+        //     ma=Math.max(ma,sum);
+        // }
+        // return ma;
+        int max = Integer.MIN_VALUE;
+        int cur = 1;
+        for (int i = 0; i < nums.length; i++) {
+            cur *= nums[i];
+            max = Math.max(max, cur);
+            if(cur == 0){
+                cur = 1;
             }
-            ma=Math.max(ma,sum);
         }
-        return ma;
+        cur = 1;
+        for (int i = nums.length-1; i >=0 ; i--) {
+            cur *= nums[i];
+            max = Math.max(max, cur);
+            if(cur == 0){
+                cur = 1;
+            }
+        }
+        return max;
     }
 }
