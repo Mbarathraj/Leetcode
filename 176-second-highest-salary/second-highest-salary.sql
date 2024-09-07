7e376fa5d37dd48158857f1b9f115900717a73ac
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-07 14:56:18 +0530

    Time: 255 ms (48.97%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select max(salary) as SecondHighestSalary from Employee where salary < (select  max(salary) from Employee)
+select max(salary) as SecondHighestSalary from Employee where salary not in (select  max(salary) from Employee)
