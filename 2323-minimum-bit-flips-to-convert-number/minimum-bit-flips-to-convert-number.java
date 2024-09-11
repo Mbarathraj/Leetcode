56bd4c0a81931d13c28ec8d5194e981d52f8c84b
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-11 21:19:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (47.90%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -2,8 +2,12 @@ class Solution {
     public int minBitFlips(int start, int goal) {
         int num=start^goal;
         int count=0;
-        for(char c:Integer.toString(num,2).toCharArray()){
-            if(c=='1') count++;
+        // for(char c:Integer.toString(num,2).toCharArray()){
+        //     if(c=='1') count++;
+        // }
+        while(num>0){
+            num=num & (num-1);
+            count++;
         }
         return count;
     }
