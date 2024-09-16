0eff37e1b3c9575e2b7922404f2337a7d5e51953
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-16 21:40:57 +0530

    Time: 2 ms (98.49%) | Memory: 44.9 MB (93.58%) - LeetSync

---
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findMinDifference(List<String> tp) {
+        boolean a[]=new boolean[1440];
+        for(String t:tp){
+            int h=Integer.parseInt(t.substring(0,2));
+            int min=Integer.parseInt(t.substring(3));
+            h=h*60+min;
+            if(a[h]) return 0;
+            a[h]=true;
+        }
+        int prev=-1;
+        int fValue=-1;
+        int m=Integer.MAX_VALUE;
+        for(int cur=0;cur<1440;cur++){
+            if(a[cur]){
+                if(prev==-1) {
+                    prev=cur;
+                    fValue=cur;
+                }
+                 else{
+                 m=Math.min(m,cur-prev);
+                 prev=cur;
+            }
+            }
+        }
+        if(prev!=-1) m=Math.min(m,1440 + fValue - prev);
+        return m;
+    }
+}
