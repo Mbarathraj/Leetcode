fea0db91eb93024c1b37d3e818cab99829190c9b
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-19 22:45:21 +0530

    Time: 1 ms (99.68%) | Memory: 42 MB (69.26%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,9 +1,11 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
-        return find(expression,0,expression.length()-1);
+        ArrayList<Integer> [][]dp=new ArrayList[expression.length()][expression.length()];
+        return find(expression,0,expression.length()-1,dp);
     }
-    public List<Integer> find(String exp, int st,int en){
+    public List<Integer> find(String exp, int st,int en,List<Integer> [][]dp){
         List<Integer> res=new ArrayList<>();
+        if(dp[st][en]!=null) return dp[st][en];
         if(st==en) {
             res.add(exp.charAt(st)-'0');
             return res;
@@ -16,8 +18,8 @@ class Solution {
             if(Character.isDigit(exp.charAt(k))) continue;
             else{
                 char op=exp.charAt(k);
-                List<Integer> left=find(exp,st,k-1);
-                List<Integer> right=find(exp,k+1,en);
+                List<Integer> left=find(exp,st,k-1,dp);
+                List<Integer> right=find(exp,k+1,en,dp);
                 for(int i:left){
                     for(int j:right){
                         if(op=='+'){
@@ -34,6 +36,7 @@ class Solution {
             } 
     
         }
+        dp[st][en]=res;
             return res;
     }
 }
