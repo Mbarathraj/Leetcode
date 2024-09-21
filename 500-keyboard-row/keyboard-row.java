1633f5392e014656409328f1f0dd8918e856dbe4
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-21 22:27:31 +0530

    Time: 2 ms (19.43%) | Memory: 42 MB (9.36%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String f="qwertyuiop";
+        String s="asdfghlkj";
+        String t="zxcvbnm";
+        ArrayList<String> li=new ArrayList<>();
+        for(String word:words){
+            int fr=0,sr=0,tr=0;
+            for(char c:word.toCharArray()){
+                if(f.contains(""+Character.toLowerCase(c))) fr++;
+                else if(s.contains(""+Character.toLowerCase(c))) sr++;
+                else tr++;
+            }
+            if(fr>0 && sr>0 || fr>0 && tr>0 || sr>0 && tr>0) continue;
+            else li.add(word);
+            
+        }
+            String[] arr = li.stream()
+                 .map(String::valueOf)
+                 .toArray(String[]::new);
+
+          return arr;
+    }
+}
