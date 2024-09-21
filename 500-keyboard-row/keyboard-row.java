6b40d6f757b3bc6b85f47b5d252ccb071c4c8f4b
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-21 22:32:17 +0530

    Time: 1 ms (54.55%) | Memory: 41.3 MB (58.23%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -1,14 +1,15 @@
 class Solution {
     public String[] findWords(String[] words) {
-        String f="qwertyuiop";
-        String s="asdfghlkj";
-        String t="zxcvbnm";
+        String f="qwertyuiopQWERTYUIOP";
+        String s="asdfghjklASDFGHJKL";
+        String t="zxcvbnmZXCVBNM";
         ArrayList<String> li=new ArrayList<>();
+        int a[]=new int[words.length];
         for(String word:words){
             int fr=0,sr=0,tr=0;
             for(char c:word.toCharArray()){
-                if(f.contains(""+Character.toLowerCase(c))) fr++;
-                else if(s.contains(""+Character.toLowerCase(c))) sr++;
+                if(f.contains(""+c)) fr++;
+                else if(s.contains(""+c)) sr++;
                 else tr++;
             }
             if(fr>0 && sr>0 || fr>0 && tr>0 || sr>0 && tr>0) continue;
@@ -20,7 +21,6 @@ class Solution {
           for(String st:li){
             arr[i++]=st;
           }
-
           return arr;
     }
 }
