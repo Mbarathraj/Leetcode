79f10965086b9b649c8a47e36b4dbeedf1834462
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-24 21:47:56 +0530

    Time: 3 ms (23.57%) | Memory: 41.2 MB (76.29%) - LeetSync

---
+++ 2226-rings-and-rods/rings-and-rods.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPoints(String rings) {
+        int c=0;
+        for(int i=0;i<=9;i++){
+            boolean g=false,r=false,b=false;
+            for(int j=0;j<rings.length();j++){
+                if(rings.charAt(j)-'0'==i){
+                    if(rings.charAt(j-1)=='B') b=true;
+                    else if(rings.charAt(j-1)=='G') g=true;
+                    else if(rings.charAt(j-1)=='R') r=true;
+                }
+            }
+            if(r && g && b) c++;
+        }
+        return c;
+    }
+}
