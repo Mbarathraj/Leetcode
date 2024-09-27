6dada017bf7f4a72ceec677cc63e8cc67f66496e
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-09-27 21:46:59 +0530

    Time: 308 ms (6.68%) | Memory: 46.6 MB (6.50%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.java
@@ -0,0 +1,29 @@
+class MyCalendar {
+    TreeMap<Integer,Integer> map;
+    public MyCalendar() {
+        map=new TreeMap<>();
+    }
+    
+    public boolean book(int start, int end) {
+        map.put(start,map.getOrDefault(start,0)+1);
+        map.put(end,map.getOrDefault(end,0)-1);
+        int c=0;
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            c+=entry.getValue();
+            if(c>1){
+                map.put(start,map.get(start)-1);
+                map.put(end,map.get(end)+1);
+                if(map.get(start)==0) map.remove(start);
+                if(map.get(end)==0) map.remove(end);
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
