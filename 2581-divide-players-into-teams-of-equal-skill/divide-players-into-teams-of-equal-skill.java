806c366d2a47c1e7cbb20435c5c2e249f85248ad
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-10-04 06:42:58 +0530

    Time: 3 ms (96.47%) | Memory: 55.7 MB (93.70%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -1,15 +1,32 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        Arrays.sort(skill);
-        int i=1,j=skill.length-2;
-        int prev=skill[0]+skill[skill.length-1];
-        long sum=skill[0]*skill[skill.length-1];
-        while(i<j){
-            if(skill[i]+skill[j]!=prev) return -1;
-            sum+=skill[i]*skill[j];
-            i++;
-            j--;
+        // Arrays.sort(skill);
+        // int i=1,j=skill.length-2;
+        // int prev=skill[0]+skill[skill.length-1];
+        // long sum=skill[0]*skill[skill.length-1];
+        // while(i<j){
+        //     if(skill[i]+skill[j]!=prev) return -1;
+        //     sum+=skill[i]*skill[j];
+        //     i++;
+        //     j--;
+        // }
+        // return sum;
+        int a[]=new int[1001];
+        int n=skill.length;
+        int sum=0;
+        for(int s:skill){
+            sum+=s;
+            a[s]++;
         }
-        return sum;
+        if(sum%(n/2)!=0) return -1;
+        int target=sum/(n/2);
+        long res=0;
+        for(int s:skill){
+            int in=target -s;
+            if(a[in]==0) return -1;
+            else res+=s*in;
+            a[in]--;
+        }
+        return res/2;
     }
 }
