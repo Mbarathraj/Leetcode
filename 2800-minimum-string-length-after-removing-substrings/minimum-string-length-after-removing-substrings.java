8aac9e239a4e6622ac189dbaad6efba6938731a3
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-10-07 07:05:33 +0530

    Time: 2 ms (97.04%) | Memory: 42.9 MB (91.72%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,15 +1,25 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(!st.isEmpty()){
-                String s1=""+st.peek()+s.charAt(i);
-                if(s1.equals("AB") || s1.equals("CD")) st.pop();
-                else st.push(s.charAt(i));
+        // Stack<Character> st=new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     if(!st.isEmpty()){
+        //         String s1=""+st.peek()+s.charAt(i);
+        //         if(s1.equals("AB") || s1.equals("CD")) st.pop();
+        //         else st.push(s.charAt(i));
 
+        //     }
+        //     else st.push(s.charAt(i));
+        // }
+        // return st.size();
+        char a[]=new char[s.length()+1];
+        int last=-1;
+        for(char c:s.toCharArray()){
+            if(last > -1 &&  (c=='B' && a[last]=='A' ||  c=='D' && a[last]=='C')) last--;
+            else{
+                 last++;
+                 a[last]=c;
             }
-            else st.push(s.charAt(i));
         }
-        return st.size();
+        return last+1;
     }
 }
