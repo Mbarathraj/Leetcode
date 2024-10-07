b56d184ff9665369a9127eaa19aac861d6dc1b5a
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-10-07 06:56:55 +0530

    Time: 13 ms (9.94%) | Memory: 45 MB (12.38%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,20 +1,15 @@
 class Solution {
     public int minLength(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        
-        boolean found;
-        do {
-            found = false;
-            for (int i = 0; i < sb.length() - 1; i++) {
-                String pair = sb.substring(i, i + 2);
-                if (pair.equals("AB") || pair.equals("CD")) {
-                    sb.delete(i, i + 2); // Remove the pair
-                    found = true; // Indicate that we found a pair to remove
-                    break; // Break to start over from the beginning
-                }
-            }
-        } while (found); // Continue until no pairs are found
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(!st.isEmpty()){
+                String s1=""+st.peek()+s.charAt(i);
+                if(s1.equals("AB") || s1.equals("CD")) st.pop();
+                else st.push(s.charAt(i));
 
-        return sb.length(); // Return the remaining length of the string
+            }
+            else st.push(s.charAt(i));
+        }
+        return st.size();
     }
 }
