56cf8bccaa9a7950c2eb1a177fb7537f73243862
Barathraj M <117514055+Mbarathraj@users.noreply.github.com>
2024-10-09 07:31:39 +0530

    Time: 50 ms (70.48%) | Memory: 48.8 MB (46.55%) - LeetSync

---
+++ 2733-sleep/sleep.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(millis);
+        }, millis);
+    });
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
